e22d7a52c2c147b0018bda72191caaf404d2b208
SSong-develop <thdgnsrl123@naver.com>
2021-05-15 17:13:21 +0900

    예진 test

---
+++ app/src/main/java/sopt/co/kr/millielibraryandroid/util/ContextExtension.kt
@@ -0,0 +1,8 @@
+package sopt.co.kr.millielibraryandroid.util
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.shortToast(message : String){
+    Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+}
